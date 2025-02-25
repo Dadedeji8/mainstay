b43a17b0c093c3aa492f53b0ce85ea25d9096650
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2025-02-25 19:24:46 +0100

    remove hover effect from transaction tabs

--- src/layouts/makeTransactions/MakeTransaction.jsx
+++ src/layouts/makeTransactions/MakeTransaction.jsx
@@ -25,12 +25,13 @@ const MakeTransaction = () => {
             <DashboardNavbar />
             <Card sx={{ padding: 5 }}>
                 <div className="outline rounded-lg flex outline-gray-300">
-                    <div className={`withdraw rounded-l-lg flex-1 outline outline-gray-300 flex hover:bg-gray-100 p-2 cursor-pointer hover:text-black justify-center items-center ${activeTab ? 'bg-[#1A73E8] text-white' : ''}`} onClick={() => setActiveTab(true)}>
+                    {/* NOTE: i remove the hover effect */}
+                    <div className={`withdraw rounded-l-lg flex-1 outline outline-gray-300 flex  p-2 cursor-pointer hov er:text-black justify-center items-center ${activeTab ? 'bg-[#1A73E8] text-white' : ''}`} onClick={() => setActiveTab(true)}>
                         <p>
                             Withdraw
                         </p>
                     </div>
-                    <div className={`withdraw flex-1 rounded-r-lg outline outline-gray-300 cursor-pointer flex p-2 hover:bg-gray-100 hover:text-black justify-center items-center ${!activeTab ? 'bg-[#1A73E8] text-white' : ''}`} onClick={() => setActiveTab(false)}>
+                    <div className={`withdraw flex-1 rounded-r-lg outline outline-gray-300 cursor-pointer flex p-2 hov er:bg-gray-100 justify-center items-center ${!activeTab ? 'bg-[#1A73E8] text-white' : ''}`} onClick={() => setActiveTab(false)}>
                         <p>
                             Deposit
                         </p>
